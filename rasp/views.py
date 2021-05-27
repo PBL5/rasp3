2e0bdd1bbe8d636601a82b424583968a814def5c
lioaslan <arey.tieuleo.5.7@gmail.com>
2021-05-27 16:51:28 +0700

    Init view, sth..

--- rasp/views.py
+++ rasp/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+#  from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.response import Response
+
 
 # Create your views here.
+class PictureAPIView(generics.GenericAPIView):
+    def get(self, request):
+        return Response()
