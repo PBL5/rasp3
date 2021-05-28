d32c94ca3476c9574e808f1178bd824faa270a2f
lioaslan <arey.tieuleo.5.7@gmail.com>
2021-05-28 12:58:10 +0700

    Init view

--- rasp/urls.py
+++ rasp/urls.py
@@ -1,5 +1,25 @@
 from . import views
+from django.urls import path
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Student Management API",
+        default_version='v1',
+        description="Document for API",
+        terms_of_service="https://www.pbl5.com/policies/terms/",
+        contact=openapi.Contact(email="contact@snippets.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny, ),
+)
 
 urlpatterns = [
-    path('picture/', views.PictureAPIView)
+    path('capture/', views.PictureAPIView),
+    path('doc',
+         schema_view.with_ui('swagger', cache_timeout=0),
+         name='schema-swagger-ui'),
 ]
--- rasp/views.py
+++ rasp/views.py
@@ -1,9 +1,12 @@
 #  from django.shortcuts import render
 from rest_framework import generics
 from rest_framework.response import Response
+from PIL import Image
 
 
 # Create your views here.
 class PictureAPIView(generics.GenericAPIView):
     def get(self, request):
+        img = Image.open('blackpink.jpg')
+        print(img)
         return Response()
